6a5b1f3783122a50d914fa3b92b709a3ebe92dd9
Andrew Neitsch <andrew@neitsch.ca>
2012-03-09 05:04:38 +0700

    Add TopUnknown analysis from 2012-02-28.

---
+++ src/net/subjoin/mosd/reload/TopUnknown.java
@@ -0,0 +1,26 @@
+package net.subjoin.mosd.reload;
+
+import java.util.Map;
+
+import net.subjoin.mosd.LanguageClassifier;
+import net.subjoin.mosd.SourcePackage;
+import net.subjoin.mosd.UbuntuDistribution;
+
+public class TopUnknown
+{
+    
+    private UbuntuDistribution _ub; 
+    private Map<String, SourcePackage> _spl;
+    
+    public TopUnknown(UbuntuDistribution ub, Map<String, SourcePackage> sp)
+    {
+	_ub = ub;
+	_spl = sp;
+	
+	LanguageClassifier cl = new LanguageClassifier();
+	for (SourcePackage p: _spl.values()) {
+	    cl.classify(p.iterateSourceFiles());
+	}
+	System.out.println(cl.getTopUnknown(25));
+    }
+}
